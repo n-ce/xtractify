29c6d8c2be8424a3de1df2e3d0fdbe2729a198ff
Enceladus <69345507+n-ce@users.noreply.github.com>
2022-12-19 03:34:42 +0000

    reduced code using query params

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import bottle
+from bottle import route, request, response, template
 import yt_dlp
 
-@bottle.route('/<query>')
-def index(query):
-  return '<!DOCTYPE html><html><audio controls src=\"'+ yt_dlp.YoutubeDL({'format':query.split("+")[1]}).extract_info("https://www.youtube.com/watch?v="+query.split("+")[0])["url"]+'\"></audio></html>' 
+@bottle.route('/')
+def index():
+    return '<audio controls src=\"' + yt_dlp.YoutubeDL({'format':request.query.format}).extract_info(request.query.id)["url"] + '\"></audio>'
 
 bottle.run(host='0.0.0.0', port=1234)
