0a0b972957c47e1a3e9821125b4c20ebf0512472
Enceladus <69345507+n-ce@users.noreply.github.com>
2022-12-23 07:31:49 +0000

    Added request header referrer

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import bottle
-from bottle import route, request, response, template
-import yt_dlp
+# A very simple Bottle Hello World app for you to get started with...
+from bottle import route, request, run
+from yt_dlp import YoutubeDL
 
-@bottle.route('/')
-def index():
-    return '<audio controls src=\"' + yt_dlp.YoutubeDL({'format':request.query.format}).extract_info(request.query.id)["url"] + '\"></audio>'
 
-bottle.run(host='0.0.0.0', port=1234)
+@route('/<a>/<b>')
+def index(a, b):
+    if request.headers['referer'] == "https://ytify.netlify.app/":
+        return YoutubeDL({
+            'format': b
+        }).extract_info('https://youtu.be/' + a, download=False)["url"]
+
+
+run(host='0.0.0.0', port='1234')
