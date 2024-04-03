90588e21847c0603a2bfdb071339b139b5ef90d6
n-ce <abrocks1120@gmail.com>
2024-04-03 22:19:44 +0530

    downgrade sanic

--- api/index.py
+++ api/index.py
@@ -1,15 +1,15 @@
 from sanic import Sanic,text
 from yt_dlp import YoutubeDL
 
-app = Sanic('xtractify')
+app = Sanic()
 
 @app.get('/')
 async def index(request):
     return text('/uid/itag')
 
 @app.get('/<id>/<itag>')
-async def main(request,id,itag):
+async def main(request,id='',itag=''):
     ydl = YoutubeDL({'format': itag}).extract_info('https://youtu.be/'+id, download=False)
     if ydl :
-        return text(ydl["url"])
+        return text(ydl['url'])
 
