ea1c4d1814b33b579b07fbb9f9a8efdc962cf5b9
n-ce <abrocks1120@gmail.com>
2024-04-03 21:48:37 +0530

    enable index

--- api/index.py
+++ api/index.py
@@ -3,11 +3,13 @@ from yt_dlp import YoutubeDL
 
 app = Sanic('xtractify')
 
+@app.get('/')
+async def index(request):
+    return text('/uid/itag')
+
 @app.get('/<id>/<itag>')
-async def index(request,id,itag):
+async def main(request,id,itag):
     ydl = YoutubeDL({'format': itag}).extract_info('https://youtu.be/'+id, download=False)
     if ydl :
         return text(ydl["url"])
-
-# api/<youtube-id>/<itag-required> returns an authenticated url string
 
