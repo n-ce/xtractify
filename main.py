fe925ad3508ff119754e1e3e1c27facc99c68e15
n-ce <abrocks1120@gmail.com>
2024-04-04 12:14:55 +0530

    fix deploy

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from sanic import Sanic,text
 from yt_dlp import YoutubeDL
 
-app = Sanic()
+app = Sanic('xtractify')
 
 @app.get('/')
 async def index(request):
     return text('/uid/itag')
 
 @app.get('/<id>/<itag>')
-async def main(request,id='',itag=''):
+async def main(request,id,itag):
     ydl = YoutubeDL({'format': itag}).extract_info('https://youtu.be/'+id, download=False)
     if ydl :
         return text(ydl['url'])
