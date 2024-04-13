c9a1101be9e158a6fd05db5c8f4d07af6cfbc478
n-ce <abrocks1120@gmail.com>
2024-04-13 13:05:58 +0530

    enable cors

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from sanic import Sanic,text
 from yt_dlp import YoutubeDL
+from cors import add_cors_headers
+from options import setup_options
+
 
 app = Sanic('xtractify')
 
 @app.get('/')
 async def index(request):
-    return text('/uid/itag')
+    return text('Please enter a 11 letter id followed by a 3 digit itag /uid/itag')
 
 @app.get('/<id>/<itag>')
 async def main(request,id,itag):
@@ -13,3 +16,8 @@ async def main(request,id,itag):
     if ydl :
         return text(ydl['url'])
 
+# Add OPTIONS handlers to any route that is missing it
+app.register_listener(setup_options, "before_server_start")
+
+# Fill in CORS headers
+app.register_middleware(add_cors_headers, "response")
